520572f15e7f47968a1ff4306624177328e6e098
KelpieLW <james.weisnner@mail.escuelaing.edu.co>
2025-11-07 02:53:03 -0500

    Solving CORS issue with connections from different port

--- src/main/java/com/Highspring/ShoppingCart/controller/OrderItemController.java
+++ src/main/java/com/Highspring/ShoppingCart/controller/OrderItemController.java
@@ -22,7 +22,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost")
 @RequestMapping("/v1/api/orderItems")
 @RequiredArgsConstructor
 @Tag(name = "Order Items", description = "Manage items in a user's order or shopping cart")
